7b8584d8292ff87a6e0b5767a2c64a3c494c97b7
sguha-work <sguha1988.life@gmail.com>
2018-10-15 10:55:46 +0530

    reset functionality done

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ var Game = /** @class */ (function () {
         this.moveCounter = 1;
     }
     Game.prototype.displayZero = function (element) {
-        var imageElement = '<img style="height: 100px; width: 100px;" src="zero.jpg"/>';
+        var imageElement = '<img style="height: ' + Game.imageHeight + '; width: ' + Game.imageWidth + ';" src="zero.jpg"/>';
         element.html(imageElement);
         element.attr("data-marked", "true");
     };
     Game.prototype.displayCross = function (element) {
-        var imageElement = '<img style="height: 100px; width: 100px;" src="cross.jpg"/>';
+        var imageElement = '<img style="height: ' + Game.imageHeight + '; width: ' + Game.imageWidth + ';" src="cross.jpg"/>';
+        ;
         element.html(imageElement);
         element.attr("data-marked", "true");
     };
+    Game.prototype.horizontalCheck = function () {
+        return false;
+    };
+    Game.prototype.verticalCheck = function () {
+        return false;
+    };
+    Game.prototype.diagonalCheck = function () {
+        return false;
+    };
     Game.prototype.check = function () {
+        return this.verticalCheck() || this.horizontalCheck() || this.diagonalCheck();
+    };
+    Game.prototype.reset = function () {
+        $("#table_game tr td img").remove();
+        $("#table_game tr td").attr('data-marked', 'false');
+        this.moveCounter = 1;
     };
     Game.prototype.move = function (element) {
         if (element.attr('data-marked') != 'true') {
@@ -31,11 +47,16 @@ var Game = /** @class */ (function () {
             }
         }
     };
+    Game.imageHeight = "90px";
+    Game.imageWidth = "90px";
     return Game;
 }());
 $(document).ready(function () {
     var gameObject = new Game();
     $("#table_game tr td").on('click', function (event) {
         gameObject.move($(event.currentTarget));
+    });
+    $("button").on('click', function () {
+        gameObject.reset();
     });
 });
--- app.ts
+++ app.ts
@@ -1,20 +1,40 @@
 declare var jQuery, $;
 class Game {
+    private static imageHeight: string = "90px";
+    private static imageWidth: string = "90px";
     public moveCounter = 1;
     private displayZero(element: any) {
-        let imageElement = '<img style="height: 100px; width: 100px;" src="zero.jpg"/>';
+        let imageElement = '<img style="height: '+Game.imageHeight+'; width: '+Game.imageWidth+';" src="zero.jpg"/>';
             element.html(imageElement);
             element.attr("data-marked", "true");
     }
 
     private displayCross(element: any) {
-        let imageElement = '<img style="height: 100px; width: 100px;" src="cross.jpg"/>';
+        let imageElement = '<img style="height: '+Game.imageHeight+'; width: '+Game.imageWidth+';" src="cross.jpg"/>';;
         element.html(imageElement);
         element.attr("data-marked", "true");
     }
 
-    private check() {
-        
+    private horizontalCheck(): boolean {
+        return false;
+    }
+
+    private verticalCheck(): boolean {
+        return false;
+    }
+
+    private diagonalCheck(): boolean {
+        return false;
+    }
+
+    private check(): boolean {
+        return this.verticalCheck() || this.horizontalCheck() || this.diagonalCheck();
+    }
+
+    public reset() {
+        $("#table_game tr td img").remove();
+        $("#table_game tr td").attr('data-marked', 'false');
+        this.moveCounter = 1;    
     }
 
     public move(element: any) {
@@ -38,6 +58,9 @@ $(document).ready(() => {
     let gameObject = new Game();
     $("#table_game tr td").on('click', (event) => {
         gameObject.move($(event.currentTarget));
+    });
+    $("button").on('click', () => {
+        gameObject.reset();
     });
 });
 
